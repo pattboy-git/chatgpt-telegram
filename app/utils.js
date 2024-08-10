faf20b7a9c170137947b0b4dfda24f95ab95157b
toptalenstar <suzukihiroshi4782@gmail.com>
2024-08-11 08:39:57 +0900

    modification of utils.js for adding start function

--- app/utils.js
+++ app/utils.js
@@ -4,6 +4,10 @@ export function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+export function onStart(user) {
+    return new Promise(resolve => setTimeout(resolve, user));
+}
+
 export function convertExponentialToSymbol(text) {
     return text.replace(/\^\d+/g, (match) => str(match));
 }
